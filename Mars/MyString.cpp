020c86505bc016b588bdd63be07debd37e91b605
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2021-04-23 09:07:42 -0500

    Mars HW work

--- Mars/MyString.cpp
+++ Mars/MyString.cpp
@@ -1,1 +1,34 @@
 // TODO: Implement this source file
+#include "MyString.h"
+
+MyString::MyString() ; theString[] {}
+    
+MyString::~MyString(){
+    delete []theString;
+}
+
+bool MyString::empty(){
+    if(size > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void MyString::clear(){
+    //somehow clears all the values and set size to zero
+}
+
+void MyString::resize(size_t newSize){
+    if(newSize > size){
+        //create new array with increased size and copy values in from old array
+    }
+    else if(newSize < size){
+        //create new array and copy in values from old array until new array is full
+    }
+    else{
+        //do nothing because the resize doesn't actually change the size
+    }
+}
+
--- Mars/MyString.h
+++ Mars/MyString.h
@@ -3,3 +3,25 @@
 #include <fstream>
 #include <tuple>
 #include <limits>
+
+class MyString{
+    private:
+        int size;
+        int capacity;
+        char* theString[];
+    public:
+        MyString(); //default constructor
+        // need copy constructor
+        // need constructor from cstring
+        ~MyString();
+
+        int size(){ return size; } // number of elements in array (length)
+        int capacity(){ return capacity; } // the storage reserved for the string in bytes
+        bool empty();// checks if the array is empty
+        void clear(); // erases all values from the array and returns the capacity and size to zero
+        void resize(size_t n); // resizes the array to capacity n
+        char* at(int i); // gives the element at index i
+        size_t find(const MyString& str, size_t pos); 
+
+
+}
