8b8d1f87d3e48fe6ff360a3ce921d7fcff4f73e2
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2021-04-30 18:02:32 -0500

    Lab 9

--- FunctionTest.cpp
+++ FunctionTest.cpp
@@ -1,67 +1,40 @@
+// "New" means new compared to previous level
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class IntNode {
-   public:
-      IntNode(int value = -1, IntNode* nextLoc = nullptr);
-      void InsertAfter(IntNode* nodeLoc);
-      int GetValue();
-      IntNode* GetNext();
-      void PrintData();
-   private:
-      int value;
-      IntNode* nextIntNodePtr;
-};
+void ScrambleNums(vector<int> remainNums, vector<int> scramNums) {
+   vector<int> tmpRemainNums;
+   int tmpRemovedNum;
+   int i;
 
-IntNode::IntNode(int val, IntNode* nextLoc) {
-   this->value = val;
-   this->nextIntNodePtr = nextLoc;
-}
-
-void IntNode::InsertAfter(IntNode* nodeLoc) {
-   IntNode* tmpNext = nullptr;
-
-   tmpNext = this->nextIntNodePtr;
-   this->nextIntNodePtr = nodeLoc;
-   nodeLoc->nextIntNodePtr = tmpNext;
-}
-
-IntNode* IntNode::GetNext() {
-   return this->nextIntNodePtr;
-}
-
-void IntNode::PrintData() {
-   cout << this->value << endl;
+   if (remainNums.size() == 0) {
+      cout << scramNums.at(0);
+      cout << scramNums.at(1);
+      cout << scramNums.at(2) << endl;
+   }
+   else {
+      for (i = remainNums.size() - 1; i >= 0; --i) { // New: This line changed
+         tmpRemainNums = remainNums; // Make a copy.
+         tmpRemovedNum = tmpRemainNums.at(i);
+         tmpRemainNums.erase(tmpRemainNums.begin() + i); // Remove element at index i
+         scramNums.push_back(tmpRemovedNum);
+         ScrambleNums(tmpRemainNums, scramNums);
+         scramNums.erase(scramNums.end() - 1); // Remove last element
+      }
+   }
 }
 
 int main() {
-   IntNode* headObj = nullptr;
-   IntNode* node1 = nullptr;
-   IntNode* node2 = nullptr;
-   IntNode* node3 = nullptr;
-   IntNode* node4 = nullptr;
-   IntNode* currObj = nullptr;
+   vector<int> numsToScramble;
+   vector<int> resultNums;
 
-   headObj = new IntNode(-1);
+   numsToScramble.push_back(5);
+   numsToScramble.push_back(1);
+   numsToScramble.push_back(8);
 
-   node1 = new IntNode(1);
-   headObj->InsertAfter(node1);
-
-   node2 = new IntNode(2);
-   node1->InsertAfter(node2);
-
-   node3 = new IntNode(3);
-   node1->InsertAfter(node3);
-
-   node4 = new IntNode(4);
-   node2->InsertAfter(node4);
-
-   currObj = headObj;
-
-   while (currObj != nullptr) {
-      currObj->PrintData();
-      currObj = currObj->GetNext();
-   }
+   ScrambleNums(numsToScramble, resultNums);
 
    return 0;
 }
---
+++ LW9/linked_list.cpp
@@ -0,0 +1,81 @@
+#include "linked_list.h"
+#include <iostream>
+#include <string>
+using std::cout, std::endl, std::string, std::ostream;
+
+void MyList::add(string name, int score) {
+    // TODO
+    //made new node with inputted vales
+    MyNode* newNode = new MyNode(name, score);
+    //assign the old last node's next node ptr to the new node
+    _tailPtr->setNext(newNode);
+    //reassign the last node pointer to the new node
+    _tailPtr = newNode;
+
+}
+
+void MyList::clear() {
+    // TODO
+}
+
+bool MyList::remove(string name) {
+    // TODO
+    return false;
+}
+
+bool MyList::insert(string name, int score, size_t index) {
+    // TODO
+    return false;
+}
+
+MyList::MyList()
+  : _size(0), _headPtr(nullptr), _tailPtr(nullptr) { }
+
+MyList::~MyList() {
+    clear();
+}
+
+size_t MyList::size() {
+    return _size;
+}
+
+bool MyList::empty() {
+    return _headPtr == nullptr;
+}
+
+MyNode* MyList::head() const {
+    return _headPtr;
+}
+
+ostream& operator<<(ostream& os, const MyList& myList) {
+    MyNode* _current = myList.head();
+    if (_current == nullptr) {
+        os << "Linked list is empty" << endl;
+        return os;
+    }
+
+    while (_current != nullptr) {
+        os << "[ " << _current->name() << ", " << _current->score() << " ]" << endl;
+        _current = _current->next();
+    }
+    return os;
+}
+
+MyNode::MyNode(std::string name, int score)
+    : _name(name), _score(score), _nextPtr(nullptr) {}
+
+std::string MyNode::name()  {
+    return _name;
+}
+
+int MyNode::score() {
+    return _score;
+}
+
+void MyNode::setNext(MyNode* other) {
+    _nextPtr = other;
+}
+
+MyNode* MyNode::next() {
+    return _nextPtr;
+}
